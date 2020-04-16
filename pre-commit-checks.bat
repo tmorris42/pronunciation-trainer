0da72462539c8698695c7027733e0132c236379a
tmorris <tmorris@student.42.fr>
2020-04-16 14:40:11 +0200

    Rename speechrecog.py -> pronunciation_trainer.py

--- pre-commit-checks.bat
+++ pre-commit-checks.bat
@@ -2,9 +2,9 @@ ECHO OFF
 python -m isort -q --skip .\env\
 python -m black -q --line-length 79 .
 python -m mypy --warn-unused-ignores .
-python -m flake8 .
+python -m flake8 pronunciation_trainer.py
 python -m flake8 .\tests\
-python -m pylint speechrecog.py
-python -m pylint .\tests\test_speechrecog.py
+python -m pylint pronunciation_trainer.py
+python -m pylint .\tests\test_pronunciation_trainer.py
 python -m pytest -q
 ECHO ON
