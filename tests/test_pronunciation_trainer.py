56e6244cc79e097126557973e1931a90905cdde0
tmorris <tmorris@student.42.fr>
2020-04-16 15:10:58 +0200

    Add first unit test

---
+++ tests/test_pronunciation_trainer.py
@@ -0,0 +1,21 @@
+"""Tests for pronunciation_trainer.py"""
+
+import unittest
+from unittest.mock import Mock
+
+import pronunciation_trainer as pt
+from pronunciation_trainer import App
+
+
+# pylint: disable = R0201
+class TestApp(unittest.TestCase):
+    """Tests for the App class"""
+
+    def test_ulog(self):
+        """Tests for ulog"""
+        pt.tk = Mock()
+        app = App(Mock())
+        app.log = Mock()
+        app.log.set = Mock()
+        app.ulog("HelloWorld!")
+        app.log.set.assert_called_once_with("HelloWorld!")
