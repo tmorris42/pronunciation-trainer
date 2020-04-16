0ff018348a19a57af3403d00ccafee7982cc6b5e
tmorris <tmorris@student.42.fr>
2020-04-16 15:10:40 +0200

    Update pre-commit-checks.bat

--- pre-commit-checks.bat
+++ pre-commit-checks.bat
@@ -1,10 +1,11 @@
 ECHO OFF
 python -m isort -q --skip .\env\
-python -m black -q --line-length 79 .
+python -m black -q --line-length 79 pronunciation_trainer.py 
+python -m black -q --line-length 79 tests/test_pronunciation_trainer.py
 python -m mypy --warn-unused-ignores .
 python -m flake8 pronunciation_trainer.py
-python -m flake8 .\tests\
+python -m flake8 tests/
 python -m pylint pronunciation_trainer.py
-python -m pylint .\tests\test_pronunciation_trainer.py
+python -m pylint tests/test_pronunciation_trainer.py
 python -m pytest -q
 ECHO ON
