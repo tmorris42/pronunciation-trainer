818641b8bf0a5836eafbe993e116308f15156aca
tmorris <tmorris@student.42.fr>
2022-11-29 16:36:33 +0100

    chore: update pre-commit-checks

--- pre-commit-checks.bat
+++ pre-commit-checks.bat
@@ -1,12 +1,12 @@
 ECHO OFF
-python -m isort -q --skip .\env\
-python -m black -q --line-length 79 pronunciation_trainer.py 
+python -m isort -q pronunciation_trainer
+python -m black -q --line-length 79 pronunciation_trainer
 python -m black -q --line-length 79 tests/test_pronunciation_trainer.py
-python -m mypy --warn-unused-ignores .
-python -m flake8 pronunciation_trainer.py
-python -m flake8 tests/
-python -m pylint pronunciation_trainer.py
+python -m mypy --warn-unused-ignores pronunciation_trainer
+python -m flake8 pronunciation_trainer
+python -m flake8 tests
+python -m pylint pronunciation_trainer
 python -m pylint tests/test_pronunciation_trainer.py
 python -m pytest -q
-python -m bandit pronunciation_trainer.py -q
+python -m bandit -r pronunciation_trainer -q
 ECHO ON
